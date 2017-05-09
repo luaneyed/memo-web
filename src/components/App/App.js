47ebcaef58128ef16e9fa82cd9dfb91cf1f988bf
Luan Sangguk Lee <sangguk258@gmail.com>
2017-05-10 00:42:22 +0900

    added sass-loader and autoprefixer to Webpack & fixed webpack build

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,16 @@
 import React from 'react'
+import styles from './App.scss'
 
-export default () => <div>로딩 완료</div>
+export default () => (
+  <div>
+    <div className={styles.outer}>
+      out
+      <div className={styles.inner}>
+        out -> in
+      </div>
+    </div>
+    <div className={styles.inner}>
+      in
+    </div>
+  </div>
+)
